037705794aa48cbbf1c3ac9f1da0f618c4174c48
hatim-mullawala <your-email@example.com>
2026-09-15 22:40:22 +0530

    update

--- sw.js
+++
@@ -1,7 +0,0 @@
-// This tells Android "I am a valid background worker"
-self.addEventListener('fetch', function(event) {});
-
-// This automatically closes the notification when you tap on it
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-});
